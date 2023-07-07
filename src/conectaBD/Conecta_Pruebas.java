cec0a66fd60bfbb64171db25fe5dec4f8865330d
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-07-07 15:02:43 -0300

    Actualización de gitignore y conexión con BBDD MySQL.
    la tabla que se usa en el programa se crea a partir de importación de
    "productos.csv" en la BBDD.
    
    Pasos:
    	1. Establecer conexión con la BBDD
    	2. Crear objeto Statement
    	3. Ejecutar sentencia SQL usando el objeto Statement
    	4. Leer el ResultSet producido por la ejecución de la sentencia SQL
    
    Tener en cuenta que se debe usar el driver/librería específica para la
    conexión con MySQL.
    
    El formato de la url varía según el motor de BBDD que se use.

---
+++ src/conectaBD/Conecta_Pruebas.java
@@ -0,0 +1,42 @@
+package conectaBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Conecta_Pruebas {
+
+	public static void main(String[] args) {
+		try {
+			// 1. CREAR CONEXION
+			String url = "jdbc:mysql://localhost:3306/pruebas";
+			String user = "root";
+			String password = "root";
+			Connection miConexion = DriverManager.getConnection(url, user, password);
+
+			// 2. CREAR OBJETO STATEMENT
+			Statement miStatement = miConexion.createStatement();
+
+			// 3. EJECUTAR SQL
+			String query = "SELECT * FROM productos";
+			ResultSet miResultSet = miStatement.executeQuery(query);
+
+			// 4. RECORRER EL RESULTSET
+			String codigoArticulo;
+			String nombreArticulo;
+			String precio;
+
+			while (miResultSet.next()) {
+				codigoArticulo = miResultSet.getString("CÓDIGO ARTÍCULO");
+				nombreArticulo = miResultSet.getString("NOMBRE ARTÍCULO");
+				precio = miResultSet.getString("PRECIO");
+				System.out.println(codigoArticulo + " - " + nombreArticulo + " - " + precio);
+			}
+		} catch (Exception e) {
+			System.out.println("NO CONECTA!");
+			System.out.println(e);
+		}
+	}
+
+}
