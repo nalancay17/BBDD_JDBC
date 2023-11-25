a14a0fe362fbaf750409e814738240ac7615177a
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-11-25 12:17:05 -0300

    Validaciones para la creación de instancias de la clase Producto.
    Se usa la clase BigDecimal para validar la cantidad de decimales que
    puede almacenar el precio.
    
    IMPORTANTE: intentar validar usando (por ej.) expresiones regulares NO fue posible debido a los problemas de imprecisión en la representación de números de punto flotante.

--- src/main/java/com/nico/modelo/Producto.java
+++ src/main/java/com/nico/modelo/Producto.java
@@ -1,5 +1,6 @@
 package com.nico.modelo;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 
 public class Producto {
@@ -15,6 +16,10 @@ public class Producto {
 	}
 
 	public void setCodigo(String codigo) {
+		if (codigo == null)
+			throw new IllegalArgumentException("El código no puede ser nulo");
+		if (codigo.length() < 5 || codigo.length() > 10)
+			throw new IllegalArgumentException("El código debe tener entre 5 y 10 caracteres");
 		this.codigo = codigo;
 	}
 
@@ -23,6 +28,10 @@ public class Producto {
 	}
 
 	public void setNombre(String nombre) {
+		if (nombre == null)
+			throw new IllegalArgumentException("El nombre no puede ser nulo");
+		if (nombre.length() < 3 || nombre.length() > 70)
+			throw new IllegalArgumentException("El nombre debe tener entre 3 y 70 caracteres");
 		this.nombre = nombre;
 	}
 
@@ -31,6 +40,13 @@ public class Producto {
 	}
 
 	public void setPrecio(double precio) {
+		double limite = 99999999.99;
+		if (precio < 0)
+			throw new IllegalArgumentException("El precio no puede ser negativo");
+	    if (tieneMasDe2Decimales(precio))
+	        throw new IllegalArgumentException("El precio no puede tener más de 2 decimales");
+	    if (precio > limite)
+			throw new IllegalArgumentException("El precio debe ser menor o igual a " + limite);
 		this.precio = precio;
 	}
 
@@ -47,6 +63,10 @@ public class Producto {
 	}
 
 	public void setPaisOrigen(String paisOrigen) {
+		if (paisOrigen == null)
+			throw new IllegalArgumentException("El país de origen no puede ser nulo");
+		if (paisOrigen.length() < 3 || paisOrigen.length() > 70)
+			throw new IllegalArgumentException("El país de origen debe tener entre 3 y 70 caracteres");
 		this.paisOrigen = paisOrigen;
 	}
 
@@ -54,6 +74,13 @@ public class Producto {
 	public String toString() {
 		return "Producto [" + codigo + " | " + nombre + " | " + precio + " | " + fecha
 				+ " | " + paisOrigen + "]";
+	}
+
+	private boolean tieneMasDe2Decimales(double precio) {
+		BigDecimal precioBigDecimal = BigDecimal.valueOf(precio);
+	    BigDecimal decimales = precioBigDecimal.subtract(new BigDecimal(precioBigDecimal.toBigInteger()));
+	    int cantDecimales = decimales.scale();
+	    return cantDecimales > 2;
 	}
 
 }
